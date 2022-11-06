a322a9ab21c9a563aedea55a76cbaa7de43c7c69
Mara Green <maragreen42@gmail.com>
2022-11-05 20:54:58 -0700

    finished problem 3

--- school_db/views.py
+++ school_db/views.py
@@ -148,8 +148,12 @@ SELECT `school_db_instructor`.`id`,
 # (Do not hard code his name in the print)
 def problem_three(request):
 
-  instructor_name = Instructor.objects.get(pk=2)
-  print(f'Instructor Name: {instructor_name}')
+  instructor =Instructor.objects.get(pk=2)
+  courses = Course.objects.filter(instructor_id=2)
+  print(f'Instructor Name: {instructor.first_name} {instructor.last_name}')
+  print('Courses:')
+  for course in courses:
+    print(f"""    -{course.name}""")
 
   return complete(request)
 
